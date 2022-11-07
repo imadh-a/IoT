35a14b6f79cbc7f90fcf233ebe4611a4d8ed7036
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-11-08 00:03:57 +0530

    Update blink.ino

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,25 +1,10 @@
-#define pin1 13
-#define pin2 11
-#define pin3 9
-void setup() {
-  pinMode(pin1,OUTPUT);
-  pinMode(pin2,OUTPUT);
-  pinMode(pin3,OUTPUT);}
-  
-
+void setup(){
+  pinMode(13,OUTPUT);
+}
 
 void loop() {
-  digitalWrite(pin1,HIGH);
+  digitalWrite(13,HIGH);
   delay(1000);
-  digitalWrite(pin1,LOW);
+  digitalWrite(13,LOW);
   delay(1000);
-  digitalWrite(pin2,HIGH);
-  delay(1000);
-  digitalWrite(pin2,LOW);
-  delay(1000);
-  digitalWrite(pin3,HIGH);
-  delay(1000);
-  digitalWrite(pin3,LOW);
-  delay(1000);
-
 }
