138dd83cfc72d4a872d10e13b3be8b3c092cb8d6
imadh-a <93546632+imadh-a@users.noreply.github.com>
2022-11-07 10:32:29 +0530

    Add files via upload

---
+++ TRAFFIC/TRAFFIC.ino
@@ -0,0 +1,25 @@
+#define pin1 13
+#define pin2 11
+#define pin3 9
+void setup() {
+  pinMode(pin1,OUTPUT);
+  pinMode(pin2,OUTPUT);
+  pinMode(pin3,OUTPUT);}
+  
+
+
+void loop() {
+  digitalWrite(pin1,HIGH);
+  //delay(1000);
+  digitalWrite(pin1,LOW);
+  delay(1000);
+  digitalWrite(pin2,HIGH);
+  delay(1000);
+  digitalWrite(pin2,LOW);
+  delay(1000);
+  digitalWrite(pin3,HIGH);
+  delay(1000);
+  digitalWrite(pin3,LOW);
+  delay(1000);
+
+}
