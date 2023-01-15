d182c40dec0da21631ef532c9e4d4920ec3a8dea
imadh-a <93546632+imadh-a@users.noreply.github.com>
2023-01-16 01:57:53 +0530

    Update traffic_simulation.ino

--- TRAFFIC/traffic_simulation.ino
+++ TRAFFIC/traffic_simulation.ino
@@ -1,20 +1,34 @@
-void setup() {
-  pinMode(9,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(8,OUTPUT);
+void setup()
+{
+  pinMode(11, OUTPUT);
+   pinMode(9, OUTPUT);
+   pinMode(13, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(9,HIGH);
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); 
+  digitalWrite(11, LOW);
   delay(1000);
-  digitalWrite(9,LOW);
+  digitalWrite(9, LOW);
   delay(1000);
-  digitalWrite(12,HIGH);
+  
+  
+  digitalWrite(13, LOW);
   delay(1000);
-  digitalWrite(12,LOW);
+  digitalWrite(9, HIGH);
+  delay(1000); 
+  digitalWrite(11, LOW);
   delay(1000);
-  digitalWrite(8,HIGH);
+  
+  digitalWrite(9, LOW);
   delay(1000);
-  digitalWrite(8,LOW);
+  digitalWrite(11, HIGH);
   delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+  
+       
 }
+
